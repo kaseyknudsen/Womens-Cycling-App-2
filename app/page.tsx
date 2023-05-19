9c997163ef951526995f23b6464067c2cd8a6902
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-05-19 12:36:02 -0700

    halfway through hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Navbar from "@/components/Navbar";
 import Link from "next/link";
+import HeroSection from "@/components/HeroSection";
 
 export default function HomePage() {
   return (
-    <main className="">
-      <div></div>
-    </main>
+    
+      <div>
+        <HeroSection />
+      </div>
+    
   );
 }
---
+++ components/HeroSection.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+const HeroSection = () => {
+  const header: string =
+    "Everything you want to know about your favorite cyclists, races, teams, and more.";
+  return (
+    <>
+      <div className="container flex flex-col-reverse md:flex-row items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0">
+        {/* left item */}
+        <div className="flex flex-col mb-32 md:space-y-10 md:w-1/2">
+          <h1 className="text-3xl font-bold text-center md:text-left max-w-md ">
+            {header}
+          </h1>
+          <p className="max-w-sm text-center text-darkGreyishBlue md:text-left ">
+            Find out about the latest race stats and winners.
+          </p>
+          <div className="flex justify-center md:justify-start">
+            <Link
+              href="/races"
+              className="p-2 px-6 bg-brightRedLight text-white rounded-full hover:bg-brightRed"
+            >
+              Races
+            </Link>
+          </div>
+        </div>
+        {/* images */}
+        <div className="flex">This is the right item</div>
+      </div>
+    </>
+  );
+};
+
+export default HeroSection;
